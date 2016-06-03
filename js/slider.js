9c70b1f2d8b43ec0eba7c0a1d366dcac8133c7de
Andi Maroge <Amaroge3@gmail.com>
2016-06-03 00:36:38 -0700

    updated the comments and changed the structure of the program

--- js/slider.js
+++ js/slider.js
@@ -6,27 +6,40 @@ var $nextButton = $("#arrowRight"),
 	$prevButton = $("#arrowLeft"); 
 
 var $slider = $("#slider");
+	
+	//hide all the images
 	$slider.children().hide();
+	
+	//show only the first image
 	$slider.children().first().show();
 	
-
+	/* when the next button is clicked, call moveToNext function */ 
 	$nextButton.click(function() {	
-	$slider.children().last().after($slider.children().first());
-	$slider.children().last().hide();
-	$slider.children().first().fadeIn();
+		moveToNext($slider);
 	});
 	
-	
+	/* when previous button is clicked, call moveToPrevious function */
 	$prevButton.click(function() {
-			
-			$slider.children().first().hide();
-
-			$slider.children().first().before($slider.children().last());
-			$slider.children().first().fadeIn();
+		moveToPrevious($slider);		
 	});
-
+/* call function that changes the pointer when hovering over the left and right buttons */
 changeToPointerOnButtons();	
+
+
+
 };
+/* function to show the next image */
+function moveToNext(slider){
+	slider.children().last().after(slider.children().first());
+	slider.children().last().hide();
+	slider.children().first().fadeIn();
+}
+/* function to show the previous image */
+function moveToPrevious(slider){
+	slider.children().first().hide();
+	slider.children().first().before(slider.children().last());
+	slider.children().first().fadeIn();
+}
 /* changes the cursor of the left and right buttons to a pointer when hovering
 over the elements */
 function changeToPointerOnButtons(){
